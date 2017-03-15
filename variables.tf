4b89f9392a299d7aaf85ccc03d24eaa45a0a7ac8
Jay Gordon <jaydestro@gmail.com>
2017-03-14 20:36:16 -0400

    first commit

---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "public_key_path" {
+  description = <<DESCRIPTION
+Path to the SSH public key to be used for authentication.
+Ensure this keypair is added to your local SSH agent so provisioners can
+connect.
+
+Example: ~/.ssh/terraform.pub
+DESCRIPTION
+# default      =  "~/.ssh/id_rsa.pub"
+}
+
+variable "key_name" {
+  description = "Desired name of AWS key pair"
+  default     = "jaygordon-tf-key"
+}
+
+variable "aws_region" {
+  description = "AWS region to launch servers."
+  default     = "us-east-1"
+}
+
+# Ubuntu Xenial 16.04 LTS (x64)
+variable "aws_amis" {
+  default = {
+    eu-west-1 = "ami-971238f1"
+    us-east-1 = "ami-2757f631"
+    us-west-1 = "ami-44613824"
+    us-west-2 = "ami-7ac6491a"
+  }
+}
